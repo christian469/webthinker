8b365f27e98503048e33336ad0a17dfba70acffc
Guthib <christianisapro28@gmail.com>
2026-07-17 10:57:00 +0000

    Jul 17, 2026, 10:57 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -20,7 +20,7 @@ let circley = 50;
   // Task 1: Colour Gradient
 let count = 1;
 for ( let red = 0; red < 256; red += 35 ) {
-  fill(0, 0, red)
+  fill(red)
   circle(count*50, circley, diameter);
   count = count + 1;
 }
