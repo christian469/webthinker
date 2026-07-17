5321ed3cae198e2c19a9150d309b17882e04851d
Guthib <christianisapro28@gmail.com>
2026-07-17 10:23:21 +0000

    Jul 17, 2026, 10:23 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -7,6 +7,9 @@ function setup() {
 function draw() {
   // Recap 1: Repeating Circles
  circle(75, 200, 100)
+ circle(75, 200, 100)
+ circle(75, 200, 100)
+ circle(75, 200, 100)
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
