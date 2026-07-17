d3279ec9eef0435bc4f49c314dbc7756e5576d4c
Guthib <christianisapro28@gmail.com>
2026-07-17 09:48:39 +0000

    Jul 17, 2026, 9:48 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -27,7 +27,7 @@ function draw() {
   // triangle(100,350, 200,350, 150,50);
 
   // Challenge 3: A house using rect() function
-  rect(100, 100, 250, 200)
+  rect(50, 50, 250, 200)
 
   // Challenge 4: A rocketship using triangle() function
 
