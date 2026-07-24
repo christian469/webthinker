1bafb8dee9b445289d29d6700946719c595ac657
Guthib <christianisapro28@gmail.com>
2026-07-24 09:52:05 +0000

    Jul 24, 2026, 9:52 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,14 +1,14 @@
 // write your codes here
 function setup() {
     createCanvas(600, 600)
-    background("skyblue")
+    background("skyblue");
     noLoop();
 }
 
 function draw() {
     // print(width);
     // print(height);
-    for (let x = 0; i y = ; i++){
+    for (let x = 50; y =50;  ){
         fill()
     }
 }
