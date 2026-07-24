8dc707e1cf70e146c6291bfaf0fb1dcf575f7936
Guthib <christianisapro28@gmail.com>
2026-07-24 09:57:32 +0000

    Jul 24, 2026, 9:57 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -8,6 +8,8 @@ function setup() {
 function draw() {
     // print(width);
     // print(height);
+    let diameter = 50;
+let circley = 50;
 let count = 1;
 for ( let red = 0; red < 256; red += 35 ) {
   fill(red)
