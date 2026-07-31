74af6437aa9cc52bcaa6962e90488a868fa7cf89
Guthib <christianisapro28@gmail.com>
2026-07-31 09:39:09 +0000

    Jul 31, 2026, 9:39 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,5 +1,5 @@
 start = 5;
-shapeacolor
+shapeacolor = 0;
 function setup(){
     createCanvas(600,600)
     background(220);
@@ -10,5 +10,5 @@ function draw() {
 }
 
 function mousePressed() {
-
+    shapeacolor
 }
