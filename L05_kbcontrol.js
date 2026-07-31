3d0c1389f353b38b61893ad6a47b922091811799
Guthib <christianisapro28@gmail.com>
2026-07-31 09:55:58 +0000

    Jul 31, 2026, 9:55 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -11,12 +11,12 @@ function draw() {
     rect( width/2, height/2, size, size);
 }
 
-function keypressed(){
+function keyPressed(){
     size = 200;
 }
 
 function keyReleased() {
-    
+    size = 50;
 }
 // function mousePressed() {
 //     fill(random(0,255), random(0, 255), random(0, 255));
