28f0ef8b6670ccb757686e341ff90a0e1593e500
Guthib <christianisapro28@gmail.com>
2026-07-31 10:13:29 +0000

    Jul 31, 2026, 10:13 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -15,10 +15,10 @@ function draw() {
         circle(width/2, height/2, 100);
     }
     if (showRect){
-        rect(width/2, height/2, 100)
+        rect(width/2, height/2, 100);
     }
     if (showSquare){
-        square(width/2, height/2) 
+        square(width/2, height/2); 
     }
 }
 
