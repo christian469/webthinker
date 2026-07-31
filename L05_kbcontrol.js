c4fa4d6d4423fc996f9b7bc341ed3ab6c47de27b
Guthib <christianisapro28@gmail.com>
2026-07-31 09:54:18 +0000

    Jul 31, 2026, 9:54 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -8,7 +8,7 @@ function setup(){
 
 function draw() {
     background("skyblue");
-    rect( width/2)
+    rect( width/2, height/2, )
 }
 // function mousePressed() {
 //     fill(random(0,255), random(0, 255), random(0, 255));
