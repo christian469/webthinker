7c9ccdc07e8788d199aba52890eed717038219f6
Guthib <christianisapro28@gmail.com>
2026-08-07 10:13:45 +0000

    Aug 7, 2026, 10:13 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,37 +1,37 @@
-let size = 35;
-let colour = "white"
-xpos;
-ypos;
+// let size = 35;
+// let colour = "white"
+// xpos;
+// ypos;
 
 
-// function preload() {
+function preload() {
     
-// }
+}
 
 function setup() {
     createCanvas(400, 400);
     background("lightblue");
-    xpos = width/2;
-    ypos = height/2;
-}
+//     xpos = width/2;
+//     ypos = height/2;
+// }
 
-function draw() {
-    background("lightblue");
-     fill(colour);
-    rect(width/2, height/2, 300, 300);
-    fill("black")
-    if (keyIsDown(RIGHT_ARROW)) {
-        xpos += 1 ;
-    }
-    if (keyIsDown(LEFT_ARROW)) {
-        xpos -= 1;
-    }
-    if (keyIsDown(UP_ARROW)){
-        ypos -= 1;
-    }
-    if (keyIsDown(DOWN_ARROW)){
-        ypos += 1
-    }
-    xpos = constrain(xpos, 75, 300);
-    ypos = constrain(ypos, 50, 300);
+// function draw() {
+//     background("lightblue");
+//      fill(colour);
+//     rect(width/2, height/2, 300, 300);
+//     fill("black")
+//     if (keyIsDown(RIGHT_ARROW)) {
+//         xpos += 1 ;
+//     }
+//     if (keyIsDown(LEFT_ARROW)) {
+//         xpos -= 1;
+//     }
+//     if (keyIsDown(UP_ARROW)){
+//         ypos -= 1;
+//     }
+//     if (keyIsDown(DOWN_ARROW)){
+//         ypos += 1
+//     }
+//     xpos = constrain(xpos, 75, 300);
+//     ypos = constrain(ypos, 50, 300);
 }
