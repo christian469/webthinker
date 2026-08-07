816af269ddae0afac7055ef22718f9aebde5291a
Guthib <christianisapro28@gmail.com>
2026-08-07 10:23:28 +0000

    Aug 7, 2026, 10:23 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -17,22 +17,5 @@ function setup() {
 }
 
 function draw() {
-    background("lightblue");
-//      fill(colour);
-//     rect(width/2, height/2, 300, 300);
-//     fill("black")
-//     if (keyIsDown(RIGHT_ARROW)) {
-//         xpos += 1 ;
-//     }
-//     if (keyIsDown(LEFT_ARROW)) {
-//         xpos -= 1;
-//     }
-//     if (keyIsDown(UP_ARROW)){
-//         ypos -= 1;
-//     }
-//     if (keyIsDown(DOWN_ARROW)){
-//         ypos += 1
-//     }
-//     xpos = constrain(xpos, 75, 300);
-//     ypos = constrain(ypos, 50, 300);
+
 }
