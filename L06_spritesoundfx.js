b9c1e92d0b4078c468c20388908f31e2cdcb5feb
Guthib <christianisapro28@gmail.com>
2026-08-07 10:06:39 +0000

    Aug 7, 2026, 10:06 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -31,6 +31,6 @@ function draw() {
     if (keyIsDown(DOWN_ARROW)){
         ypos += 1
     }
-    xpos = constrain(xpos, 50, 150);
-    ypos = constrain(ypos, 50, 200);
+    xpos = constrain(xpos, 50, 300);
+    ypos = constrain(ypos, 50, 300);
 }
