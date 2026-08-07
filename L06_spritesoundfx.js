b2afe2389d015f3eb32fe1c696e7d91ce845b1d0
Guthib <christianisapro28@gmail.com>
2026-08-07 10:41:14 +0000

    Aug 7, 2026, 10:41 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -7,7 +7,7 @@ let soundSFX;
 
 function preload() {
     picoImg = loadImage('assets/pico-a.png');
-    soundSFX = loadSound('assets/pop.mp3')
+    soundSFX = loadSound('assets/pop.mp3');
 }
 
 function setup() {
