28494c21ac5b651417dfea7963b29e783b1f8127
Guthib <christianisapro28@gmail.com>
2026-08-14 10:29:05 +0000

    Aug 14, 2026, 10:29 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -10,18 +10,8 @@ function setup() {
 
 function draw() {
     circle(ballX, ballY, ballSize);
-    if (keyIsDown(RIGHT_ARROW)) {
-        ballX = ballX +5;
-    }
-    if (keyIsDown(LEFT_ARROW)) {
-        ballX = ballX -5;
-    }
-    if (keyIsDown(UP_ARROW)) {
-        ballY = ballY -5;
-    }
-    if (keyIsDown(DOWN_ARROW)) {
-        ballY = ballY +5;
-    }
+    if (ballSpeedX);
+    
 }
 
 
