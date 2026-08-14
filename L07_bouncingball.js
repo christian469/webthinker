9137eb8b9b3382e9acb5dc883cbb5a9d4fb63e38
Guthib <christianisapro28@gmail.com>
2026-08-14 09:58:17 +0000

    Aug 14, 2026, 9:58 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,11 +1,55 @@
-// function setup() {
-//     createCanvas(500, 500);
-//     background(0);
-// }
+function setup() {
+    createCanvas(500, 500);
+    background(0);
+}
 
-// function draw() {
+function draw() {
 
-// }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // let xpos;
 // let ypos;
