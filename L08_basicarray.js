0ce72e71ee4c5204f99c2db372c1029f56b43b50
Guthib <christianisapro28@gmail.com>
2026-08-21 10:28:47 +0000

    Aug 21, 2026, 10:28 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -2,10 +2,9 @@ function setup(){
     createCanvas(600, 600);
     background("black");
     fill("yellow");
-
+    ypos = height
     textSize(38);
     let ypos = height;
-    
     let foodList = ["whopper", "tacos", "ice cream", "fried chicken", "chicken rice", "chessy fries"];
     for (let count=0; count < foodList.length; count++){
         print( foodList[0] );
