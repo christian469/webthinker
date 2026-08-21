4b87cde49885832b910dd44bac9d4fb16f8886d7
Guthib <christianisapro28@gmail.com>
2026-08-21 10:03:19 +0000

    Aug 21, 2026, 10:03 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -8,5 +8,5 @@ function draw() {
 
     textSize(38);
     let ypos = 50;
-    let foodList = ["burger, tacos, ice cream, "]
+    let foodList = ["burger, tacos, ice cream, fried chicken, chicken rice"]
 }
