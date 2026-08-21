0a37c2a8b35b94e1ae84c20f30d2dbc44eaf9d1f
Guthib <christianisapro28@gmail.com>
2026-08-21 09:54:08 +0000

    Aug 21, 2026, 9:54 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -5,7 +5,7 @@ function setup(){
 function draw() {
     background("black");
     textColor(35);
-    textSize("white");
+    textSize("black");
     text("my name is christian", 50, 50);
     text("my age is 999", 50, 100);
     text("my fav activity is playing games", 50, 150);
