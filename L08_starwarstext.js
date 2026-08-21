a79d777d96d9e41f15a3100737baf11ba3103898
Guthib <christianisapro28@gmail.com>
2026-08-21 10:37:42 +0000

    Aug 21, 2026, 10:37 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -909,3 +909,25 @@ let storyText = [
     "",
     "May the Code be with you!",
 ]
+function setup(){ 
+    createCanvas(600, 600);
+    // background("black");
+    fill("yellow");
+    yPos = height / 2;
+    textSize(24);
+    textAlign(CENTER, CENTER);
+    // let foodList = ["whopper", "tacos", "ice cream", "fried chicken", "chicken rice", "chessy fries"];
+    // for (let count=0; count < foodList.length; count++){
+    //     print( foodList[0] );
+    //     text( foodList[count], 50, ypos );
+    //     ypos = ypos + 50;
+    // }
+}       
+
+function draw() {
+    background(0);
+
+    text("", width / 2, yPos);
+
+    yPos -= 0.6;
+}
