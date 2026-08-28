ca7f6501e1c777ebbc55bc5c0ea78184b0505e63
Guthib <christianisapro28@gmail.com>
2026-08-28 09:50:41 +0000

    Aug 28, 2026, 9:50 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -11,7 +11,7 @@ function setup() {
 
 function draw(){
     background("lightgray");
-    Text("bouncer!!!", Xpospos, height/2);
+    Text("bouncer!!!", Xpos, height/2);
     xos = xpos + speed;
 
     if (Xpos > width){
