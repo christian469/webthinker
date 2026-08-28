1f01c5ae75ac9cd6350d119c49501d88278c7f43
Guthib <christianisapro28@gmail.com>
2026-08-28 10:10:11 +0000

    Aug 28, 2026, 10:10 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,24 +1,34 @@
-let xpos;
-let speed;
+// let xpos;
+// let speed;
 
-function setup() {
-    createCanvas(600,600);
-    textAlign(CENTER, CENTER);
-    textSize(48);
-    xpos = width/2
-    speed = 2;
-}
+// function setup() {
+//     createCanvas(600,600);
+//     textAlign(CENTER, CENTER);
+//     textSize(48);
+//     xpos = width/2
+//     speed = 2;
+// }
 
-function draw(){
-    background("lightgray");
-    text("Bounce!", xpos, height/2);
-    xpos = xpos + speed;
+// function draw(){
+//     background("lightgray");
+//     text("Bounce!", xpos, height/2);
+//     xpos = xpos + speed;
 
-    if (xpos > width){
-        speed = speed * -1;
-    }
+//     if (xpos > width){
+//         speed = speed * -1;
+//     }
     
-    if (xpos < 0){
-        speed = speed *-1;
-    }
-}
+//     if (xpos < 0){
+//         speed = speed *-1;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
