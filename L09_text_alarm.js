cd0e6e5ca5a70c93125127882de2ee3c2655aadb
Guthib <christianisapro28@gmail.com>
2026-08-28 10:21:46 +0000

    Aug 28, 2026, 10:21 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,49 +1,43 @@
-// let xpos;
-// let speed;
+// // let xpos;
+// // let speed;
+
+// // function setup() {
+// //     createCanvas(600,600);
+// //     textAlign(CENTER, CENTER);
+// //     textSize(48);
+// //     xpos = width/2
+// //     speed = 2;
+// // }
+
+// // function draw(){
+// //     background("lightgray");
+// //     text("Bounce!", xpos, height/2);
+// //     xpos = xpos + speed;
+
+// //     if (xpos > width){
+// //         speed = speed * -1;
+// //     }
+    
+// //     if (xpos < 0){
+// //         speed = speed *-1;
+// //     }
+// // }
 
 // function setup() {
-//     createCanvas(600,600);
-//     textAlign(CENTER, CENTER);
-//     textSize(48);
-//     xpos = width/2
-//     speed = 2;
+//     createCanvas(400, 200);
+//     background("skyblue");
+//     textSize(32);
+//     textAlign(CENTER,CENTER);
 // }
 
-// function draw(){
-//     background("lightgray");
-//     text("Bounce!", xpos, height/2);
-//     xpos = xpos + speed;
+// function draw() {
+//     background("skyblue");
 
-//     if (xpos > width){
-//         speed = speed * -1;
-//     }
-    
-//     if (xpos < 0){
-//         speed = speed *-1;
-//     }
+//     let xhour = nf( hour(), 2 );
+//     let xminute =nf( minute(), 2);
+//     let xsecond = nf( second(), 2);
+//     let timeString = xhour + ":" + xminute + ":" + xsecond;
+
+
+//     text(timeString, width/2, height/2);
 // }
-
-function setup() {
-    createCanvas(400, 200);
-    background("skyblue");
-    textSize(32);
-    textAlign(CENTER,CENTER);
-}
-
-function draw() {
-    background("skyblue");
-
-    let xhour = nf( hour(), 2 );
-    let xminute =nf( minute(), 2);
-    let xsecond = nf( second(), 2);
-    let timeString = xhour + ":" + xminute + ":" + xsecond;
-
-
-    text(timeString, width/2, height/2);
-}
-
-
-
-
-
-
