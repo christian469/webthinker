01a65800399781416022ae0116795d9132ebc284
Guthib <christianisapro28@gmail.com>
2026-08-28 10:30:25 +0000

    Aug 28, 2026, 10:30 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -59,7 +59,7 @@ function setup(){
     textSize(80);
     textFont("cosmic san")
     textAlign(CENTER, CENTER)
-    countdown = ;
+    countdown = 10000000000000000000000000;
 }
 
 function draw(){
