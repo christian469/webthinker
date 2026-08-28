cf737ff3a5f2a824febad3d4556e4f734f49650e
Guthib <christianisapro28@gmail.com>
2026-08-28 10:15:31 +0000

    Aug 28, 2026, 10:15 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -36,7 +36,7 @@ function draw() {
     let xhour = nf( hour(), 2 );
     let xminute =nf( minute(), 2);
     let xseconds = nf( second(), 2);
-    let timeString = xhouor + ":"
+    let timeString = xhouor + ":" + xminute
 }
 
 
