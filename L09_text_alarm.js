672aa5eaacd45b4af0760f69f914e024fc0dcad2
Guthib <christianisapro28@gmail.com>
2026-09-04 10:22:54 +0000

    Sep 4, 2026, 10:22 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,87 +1,110 @@
-// // let xpos;
-// // let speed;
+// // // // let xpos;
+// // // // let speed;
 
-// // function setup() {
-// //     createCanvas(600,600);
-// //     textAlign(CENTER, CENTER);
-// //     textSize(48);
-// //     xpos = width/2
-// //     speed = 2;
+// // // // function setup() {
+// // // //     createCanvas(600,600);
+// // // //     textAlign(CENTER, CENTER);
+// // // //     textSize(48);
+// // // //     xpos = width/2
+// // // //     speed = 2;
+// // // // }
+
+// // // // function draw(){
+// // // //     background("lightgray");
+// // // //     text("Bounce!", xpos, height/2);
+// // // //     xpos = xpos + speed;
+
+// // // //     if (xpos > width){
+// // // //         speed = speed * -1;
+// // // //     }
+    
+// // // //     if (xpos < 0){
+// // // //         speed = speed *-1;
+// // // //     }
+// // // // }
+
+// // // function setup() {
+// // //     createCanvas(400, 200);
+// // //     background("skyblue");
+// // //     textSize(32);
+// // //     textAlign(CENTER,CENTER);
+// // // }
+
+// // // function draw() {
+// // //     background("skyblue");
+
+// // //     let xhour = nf( hour(), 2 );
+// // //     let xminute =nf( minute(), 2);
+// // //     let xsecond = nf( second(), 2);
+// // //     let timeString = xhour + ":" + xminute + ":" + xsecond;
+
+
+// // //     text(timeString, width/2, height/2);
+// // // }
+// // let csecond, cminute, chour;
+// // let ctime;
+// // let interval;
+// // let countdown;
+
+// // let alarm;
+
+// // function preload(){
+// //     alarm = loadSound('assets/Lose.mp3')
+// // }
+
+
+
+// // function setup(){
+// //     createCanvas(600,400);
+// //     textSize(80);
+// //     textFont("cosmic san")
+// //     textAlign(CENTER, CENTER)
+// //     countdown = 300;
 // // }
 
 // // function draw(){
-// //     background("lightgray");
-// //     text("Bounce!", xpos, height/2);
-// //     xpos = xpos + speed;
-
-// //     if (xpos > width){
-// //         speed = speed * -1;
-// //     }
-    
-// //     if (xpos < 0){
-// //         speed = speed *-1;
-// //     }
+// //     background('gold');
+// //     csecond = second();
+// //     cminute = minute();
+// //     chour = hour();
+// //     ctime = nf(chour,2) + " : " + nf(cminute,2) + " : " + nf(csecond,2)
+// //     text(ctime, width/ 2, height/ 2);
+// //     text(countdown, width/2, height/2 +100)
 // // }
 
-// function setup() {
-//     createCanvas(400, 200);
-//     background("skyblue");
-//     textSize(32);
-//     textAlign(CENTER,CENTER);
-// }
+// // function mousePressed(){
+// //     interval = setInterval(updateCountdown, 1000);
+// // }
 
-// function draw() {
-//     background("skyblue");
+// // function updateCountdown(){
+// //     if (countdown > 0){
+// //     countdown = countdown - 1;
+// //     }
+// //     else{
+// //         clearInterval(interval);
+// //         alarm.play();
+// //     }
+// // }
+// function setup(){
+//     createCanvas(400, 150);
+//     background(200);
 
-//     let xhour = nf( hour(), 2 );
-//     let xminute =nf( minute(), 2);
-//     let xsecond = nf( second(), 2);
-//     let timeString = xhour + ":" + xminute + ":" + xsecond;
-
-
-//     text(timeString, width/2, height/2);
-// }
-let csecond, cminute, chour;
-let ctime;
-let interval;
-let countdown;
-
-let alarm;
-
-function preload(){
-    alarm = loadSound('assets/Lose.mp3')
-}
+//     for (let i = 1; i < 5; i++){
+//         circle(75 * i, 75, 50);
 
 
+// function setup(){
+//     createCanvas(400, 150);
+//     background(200);
+
+//     for (let i = 1; i < 5; i++){
+//         circle(75 * i, 75, 50);
 
 function setup(){
-    createCanvas(600,400);
-    textSize(80);
-    textFont("cosmic san")
-    textAlign(CENTER, CENTER)
-    countdown = 300;
-}
+    createCanvas(400, 150);
+    background(200);
 
-function draw(){
-    background('gold');
-    csecond = second();
-    cminute = minute();
-    chour = hour();
-    ctime = nf(chour,2) + " : " + nf(cminute,2) + " : " + nf(csecond,2)
-    text(ctime, width/ 2, height/ 2);
-    text(countdown, width/2, height/2 +100)
-}
-
-function mousePressed(){
-    interval = setInterval(updateCountdown, 1000);
-}
-
-function updateCountdown(){
-    if (countdown > 0){
-    countdown = countdown - 1;
-    }
-    else{
-        clearInterval(interval);
-        alarm.play();
+    for (let i = 1; i < 5; i++){
+        circle(75 * i, 75, 50);
     }
 }
