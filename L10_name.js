fad5f4e7726aa192d04538073d1f55723ed23345
Guthib <christianisapro28@gmail.com>
2026-09-11 10:55:13 +0000

    Sep 11, 2026, 10:55 AM

--- L10_name.js
+++ L10_name.js
@@ -21,7 +21,7 @@ function setup(){
     colorPicker = createColorPicker("yellow");
    colorPicker.position(300, height - 50) 
    colorPicker2 = createColorPicker("white");
-   colorPicker2.position(310, height)
+   colorPicker2.position(310, height -60)
 
 }
 
