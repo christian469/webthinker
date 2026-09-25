20a08865a61e8db130db00e516f13750ce4b8ee6
Guthib <christianisapro28@gmail.com>
2026-09-25 10:15:22 +0000

    Sep 25, 2026, 10:15 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -4,6 +4,7 @@ let btnGenStory
 function setup(){
     createCanvas(800,700);
     background("lightgray");
+    createInput
     btnGenStory = createButton("Generate Story");
     btnGenStory.position(200, 350);
     btnGenStory.mousePressed(buttonStoryClicked)
