3166d01da697560d66d68aabbf94607dcf0d0a1d
Guthib <christianisapro28@gmail.com>
2026-09-25 10:18:18 +0000

    Sep 25, 2026, 10:18 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -5,7 +5,7 @@ function setup(){
     createCanvas(800,700);
     background("lightgray");
     inputguess= createInput("guess");
-    inputguess.position(200, 100);
+    inputguess.position(700, 100);
 
     btnGenStory = createButton("Guess");
     btnGenStory.position(200, 350);
