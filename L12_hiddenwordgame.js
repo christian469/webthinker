351da471232092e33803e6961347d6d5b042490a
Guthib <christianisapro28@gmail.com>
2026-09-25 10:14:15 +0000

    Sep 25, 2026, 10:14 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -2,6 +2,9 @@
 function setup(){
     createCanvas(800,700);
     background("lightgray");
+    btnGenStory = createButton("Generate Story");
+    btnGenStory.position(200, 350);
+    btnGenStory.mousePressed(buttonStoryClicked)
 }
 
 function draw(){
